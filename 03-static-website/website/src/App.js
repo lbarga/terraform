f7c03becfd29c9ccdff958a8d90fe5b0dee3a75e
Lucas Barga <lucasmpbarga@gmail.com>
2023-08-11 02:13:47 -0300

    add react project website

---
+++ 03-static-website/website/src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          My React with <b>Terraform.</b>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
