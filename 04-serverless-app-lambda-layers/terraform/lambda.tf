9c2332db1cde1c06ea669acd480e5a79f7308fab
Lucas Barga <lucasmpbarga@gmail.com>
2023-08-12 18:45:25 -0300

    finish add lambda layer

--- 04-serverless-app-lambda-layers/terraform/lambda.tf
+++ 04-serverless-app-lambda-layers/terraform/lambda.tf
@@ -1,3 +1,23 @@
+resource "null_resource" "build_lambda_layers" {
+  triggers = {
+    layer_build = filemd5("${local.layers_path}/package.json")
+  }
+
+  provisioner "local-exec" {
+    working_dir = local.layers_path
+    command     = "npm install --production && cd ../ && zip -9 -r --quiet ${local.layer_name} *"
+  }
+}
+
+resource "aws_lambda_layer_version" "joi" {
+  depends_on = [null_resource.build_lambda_layers]
+
+  layer_name          = "joi-layer"
+  description         = "joi: 17.3.0"
+  filename            = "${local.layers_path}/../${local.layer_name}"
+  compatible_runtimes = ["nodejs14.x"]
+}
+
 data "archive_file" "s3" {
   type        = "zip"
   source_file = "${local.lambdas_path}/s3/index.js"
@@ -13,7 +33,7 @@ resource "aws_lambda_function" "s3" {
   filename         = data.archive_file.s3.output_path
   source_code_hash = data.archive_file.s3.output_base64sha256
 
-  # layers = [aws_lambda_layer_version.joi.arn]
+  layers = [aws_lambda_layer_version.joi.arn]
 
   # environment {
   #   variables = {
