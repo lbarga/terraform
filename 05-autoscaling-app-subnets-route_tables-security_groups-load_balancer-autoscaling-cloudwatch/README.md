05fa95a48af173212ac42571156a9f238bd5563c
Lucas Barga <lucasmpbarga@gmail.com>
2023-08-13 19:11:26 -0300

    create cloudwatch

---
+++ 05-autoscaling-app-subnets-route_tables-security_groups-load_balancer-autoscaling-cloudwatch/README.md
@@ -0,0 +1,12 @@
+05-autoscaling-app-subnets-route_tables-security_groups-load_balancer
+
+# STEPS
+
+- create vpc
+- create igw
+- create subnets
+- create route table
+- create security groups
+- create load balancer
+- create auto scaling
+- create cloudwatch
---
+++ 05-autoscaling-app-subnets-route_tables-security_groups-load_balancer-autoscaling-cloudwatch/main.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_version = "1.5.4"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.10.0"
+    }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+
+  backend "s3" {
+    bucket  = "tfstate-215785215210"
+    key     = "05-autoscaling-app/terraform.tfstate"
+    region  = "us-east-1"
+    profile = "terraform"
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+resource "random_pet" "site" {
+  length = 5
+}
+
+data "aws_caller_identity" "current" {}
